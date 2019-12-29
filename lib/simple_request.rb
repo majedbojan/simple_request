dd5dfabe77fc80a6f8d4ed81f15717b51ffff803
MaJeD BoJaN <majed.abdullah@clickapps.co>
2019-12-29 18:07:34 +0300

    added change log file

--- lib/simple_request.rb
+++ lib/simple_request.rb
@@ -1,6 +1,96 @@
 require "simple_request/version"
+require 'uri'
+require 'net/http'
+require 'net/https'
+require 'json'
 
 module SimpleRequest
   class Error < StandardError; end
   # Your code goes here...
+  class Https
+    attr_reader :uri, :body, :headers, :https
+
+    def initialize(**options)
+      options.symbolize_keys!
+      @uri     = URI(options[:url])
+      @body    = options[:body]
+      @headers = options[:headers] || {}
+    end
+
+    ##-------------------------------------------- GET Request ------------------------------------------------##
+    def self.get(*args)
+      new(*args).get
+    end
+
+    def get
+      open_ssl
+      req = Net::HTTP::Get.new(uri.path, header_processed)
+      req.body = body.to_json
+      parse_response(req)
+    end
+
+    ##-------------------------------------------- POST Request -----------------------------------------------##
+    def self.post(*args)
+      new(*args).post
+    end
+
+    def post
+      open_ssl
+      req = Net::HTTP::Post.new(uri.path, header_processed)
+      req.body = body.to_json
+      parse_response(req)
+    end
+
+    ##-------------------------------------------- PUT Request ------------------------------------------------##
+    def self.put(*args)
+      new(*args).put
+    end
+
+    def put
+      open_ssl
+      req = Net::HTTP::Put.new(uri.path, header_processed)
+      req.body = body.to_json
+      parse_response(req)
+    end
+
+    ##-------------------------------------------- DELETE Request ---------------------------------------------##
+    def self.destroy(*args)
+      new(*args).destroy
+    end
+
+    def destroy
+      open_ssl
+      req = Net::HTTP::Delete.new(uri.path, header_processed)
+      req.body = body.to_json
+      parse_response(req)
+    end
+
+  ##---------------------------------------------------------------------------------------------------------##
+
+    private
+
+    def open_ssl
+      @https = Net::HTTP.new(uri.host, uri.port)
+      @https.use_ssl = true
+      @https.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    end
+
+    def header_processed
+      { 'Content-Type' => 'application/json' }.merge(headers)
+    end
+
+    def parse_response(req)
+      https.request(req)
+    rescue Timeout::Error || Net::OpenTimeout
+      puts "\e[31mTime out!\e[0m"
+      'انتهاء الوقت المزامنة'
+    rescue Net::HTTPBadResponse || SocketError
+      puts "\e[31mRequest Failed!\e[0m"
+      'فشل طلب الملف'
+    rescue StandardError
+      puts "\e[31mAn unknown error occurred!\e[0m"
+      'خطا غير معروف'
+    end
+
+  end
 end
--- simple_request.gemspec
+++ simple_request.gemspec
@@ -6,18 +6,17 @@ Gem::Specification.new do |spec|
   spec.name          = "simple_request"
   spec.version       = SimpleRequest::VERSION
   spec.authors       = ["MaJeD BoJaN"]
-  spec.email         = ["majed.abdullah@clickapps.co"]
+  spec.email         = ["bojanmajed@gmail.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because RubyGems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{Simple ruby gem that helps to make HTTP and HTTPS request from ruby projects.}
+  spec.homepage      = "http://majedbojan.github.io/"
   spec.license       = "MIT"
 
-  spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
+  # spec.metadata["allowed_push_host"] = "TODO: Set to 'http://mygemserver.com'"
 
   spec.metadata["homepage_uri"] = spec.homepage
-  spec.metadata["source_code_uri"] = "TODO: Put your gem's public repo URL here."
-  spec.metadata["changelog_uri"] = "TODO: Put your gem's CHANGELOG.md URL here."
+  spec.metadata["source_code_uri"] = "https://github.com/MajedBojan/simple_request"
+  spec.metadata["changelog_uri"] = "https://github.com/MajedBojan/simple_request/blob/master/Changelog.md"
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
